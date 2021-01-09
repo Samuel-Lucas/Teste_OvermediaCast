72ad9ca1290a35160f07120cf3e04fdfaa013bbf
Samuel-Lucas <49723833+Samuel-Lucas@users.noreply.github.com>
2021-01-09 17:55:56 -0300

    Animando itens

--- script/script.js
+++ script/script.js
@@ -1,3 +1,25 @@
-let btn = document.getElementById('item')
+let i = 1
+let item = ''
 
-console.log(btn.clientHeight)
+function transicao() {
+
+    let titulo = document.querySelector('h6')
+    titulo.classList.add('opacidade')
+}
+
+transicao()
+
+function pulo() {
+
+    item = document.querySelector('#item' + i)
+    item.classList.add('animacao')
+    i++
+
+    setTimeout(() => {
+        item.classList.remove('animacao')
+    }, 300)
+
+    i = (i > 4 ? 1 : i)
+}
+
+setInterval(pulo, 800)  
