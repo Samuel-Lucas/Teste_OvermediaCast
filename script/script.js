16a80ea2eeea799ee66277b77033bcc37946772c
Samuel-Lucas <49723833+Samuel-Lucas@users.noreply.github.com>
2021-01-10 17:39:54 -0300

    Inclusão de uma API

--- script/script.js
+++ script/script.js
@@ -1,6 +1,9 @@
 let i = 1
 let item = ''
 let titulo = document.querySelector('h6')
+let modal = document.querySelector('#modal')
+
+
 titulo.classList.add('opacidade')
 titulo.style.visibility = 'visible'
 
@@ -16,6 +19,15 @@ function transicaoItens() {
         setTimeout(transicaoItens, 250)
     } else {
         i = 1
+
+        setTimeout(() => {
+            for(let j = 1; j <= 4; j++) {
+                item = document.querySelector('#item' + j)
+                item.classList.remove('opacidade')
+            }
+        }, 450)
+        
+
         setInterval(pulo, 500)
     }
 }
@@ -23,13 +35,12 @@ function transicaoItens() {
 function pulo() {
 
     item = document.querySelector('#item' + i)
-    item.classList.remove('opacidade')
     item.classList.add('animacao')
     i++
 
     setTimeout(() => {
         item.classList.remove('animacao')
-    }, 400)
+    }, 450)
 
     if(i > 4) {
         setTimeout(() => {
@@ -40,3 +51,25 @@ function pulo() {
     }
 }
 
+async function showContent(indice) {
+
+    let tituloModal = document.querySelector('#modal h3')
+    let imagem = document.querySelector('#modal img')
+    let height = document.querySelector('#modal .height')
+    let width = document.querySelector('#modal .weight')
+
+    let api_url = 'https://pokeapi.co/api/v2/pokemon/' + indice
+    let response = await fetch(api_url)
+    let data = await response.json()
+    
+    modal.style.display = 'block'
+    tituloModal.innerHTML = data.name
+    tituloModal.style.fontWeight = 'bold'
+    imagem.src = data.sprites.front_default
+    height.innerHTML = data.height
+    width.innerHTML = data.weight
+}
+
+function closeModal() {
+    modal.style.display = 'none'
+}
